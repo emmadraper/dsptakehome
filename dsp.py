e5372b822a20f0df02885c0a7a6d64019a6a7339
Emma Draper <erd291@nyu.edu>
2021-02-03 11:36:13 -0500

    add count on number of stops to route object

--- dsp.py
+++ dsp.py
@@ -11,7 +11,7 @@ class main():
     # Question 1
     # go through each entry to get the long_name
     def first_question(data):
-        long_names=[]
+        long_names = []
         for row in data:
             long_names += [row['attributes']['long_name']]
         print(json.dumps(long_names))
@@ -20,24 +20,23 @@ class main():
     q1 = first_question(data)
 
     # Question 2
-    # subway name with the most stops and number of stops
-    # subway route with the least stops and number of stops
+    # subway name with the most stops and number of stops https://api-v3.mbta.com/stops
+    # subway route with the least stops and number of stops https://api-v3.mbta.com/stops
     # A list of the stops that connect two or more subway routes along with the relevant route
     # names for each of those stops
     def second_question(data):
-        routes=[]
+        routes= []
         for route_entry in data:
-        	stops=requests.get('https://api-v3.mbta.com/stops?filter[route]=' + route_entry["id"])
+        	stops = requests.get('https://api-v3.mbta.com/stops?filter[route]=' + route_entry["id"])
         	stops = stops.json()["data"]
         	route_object={
-        	  "route":route_entry["attributes"]["long_name"],
-        	  "stops":[]
+        	  "route": route_entry["attributes"]["long_name"],
+        	  "stops": [],
+        	  "count": len(stops)
         	}
-        	for s in stops:
-        		route_object["stops"] += [s["attributes"]["name"]]
-        	routes+=[route_object]
+                for s in stops:
+                    route_object["stops"] += [s["attributes"]["name"]]
+                routes+=[route_object]
         print(json.dumps(routes))
-
         return
-
     q2 = second_question(data)
