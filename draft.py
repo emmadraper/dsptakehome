d577056c9e9b0af96ae9438eb5f71e18f4616316
Emma Draper <erd291@nyu.edu>
2021-02-04 03:59:26 -0500

    wip

--- draft.py
+++ draft.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import inquirizer
 
 class main():
     # call the filtered API
@@ -41,5 +42,73 @@ class main():
 
     q2 = get_direction_dest(json_data)
 
+
+# List a rail route you could travel to get from one stop to the other.
+# Davis to Kendall -> Redline
+# Ashmont to Arlington -> Redline, Greenline
+def third_question(second_question, data):
+    start = [
+        inquirer.List('long_name',
+                      message="What is your starting station?",
+                      choices=["Red Line",
+                               "Orange Line",
+                               "Blue Line",
+                               "Fairmount Line",
+                               "Fitchburg Line",
+                               "Framingham/Worcester Line",
+                               "Franklin Line",
+                               "Greenbush Line",
+                               "Haverhill Line",
+                               "Kingston/Plymouth Line",
+                               "Lowell Line",
+                               "Middleborough/Lakeville Line",
+                               "Needham Line",
+                               "Newburyport/Rockport Line",
+                               "Providence/Stoughton Line",
+                               "Foxboro Event Service"],
+                      ),
+    ]
+    end = [
+        inquirer.List('long_name',
+                      message="What is your ending station?",
+                      choices=["Red Line",
+                               "Orange Line",
+                               "Blue Line",
+                               "Fairmount Line",
+                               "Fitchburg Line",
+                               "Framingham/Worcester Line",
+                               "Franklin Line",
+                               "Greenbush Line",
+                               "Haverhill Line",
+                               "Kingston/Plymouth Line",
+                               "Lowell Line",
+                               "Middleborough/Lakeville Line",
+                               "Needham Line",
+                               "Newburyport/Rockport Line",
+                               "Providence/Stoughton Line",
+                               "Foxboro Event Service"],
+                      ),
+    ]
+    start = inquirer.prompt(start)
+    end = inquirer.prompt(end)
+    # now I have start and target station, and all I need now is to pull the stops for all stations
+    # and cross those against my starting and ending stations to figure out how to get from point A to B
+    stops = second_question(data) # can use second method for this
+    connected_paths = []
+    known_path=[]
+    for stops in connected_paths:
+        # check if the current subway can take us directly to target
+        if end in stops["stops"]:
+            known_path += already_path + [stops]
+            continue
+        #now check other stops to see if they can take us to the target
+        for end in stops["stops"]:
+            if end == start: # no need to check out current stop
+                continue
+            else:
+                # let check the this stop for connections
+                known_path += find_next_route(stops, target, aready_path + [stops])
+    return known_path
+    print (len(route))
 
 
--- dsp.py
+++ dsp.py
@@ -1,6 +1,5 @@
 import json
 import requests
-import inquirer
 
 class main():
     # call the filtered API
@@ -36,70 +35,27 @@ class main():
                 routes+=[route_object]
         maxstops = max(routes, key=lambda x:x['count'])
         minstops = min(routes, key=lambda x:x['count'])
-        print(json.dumps(maxstops))
-        print(json.dumps(minstops))
-
+        #print(json.dumps(maxstops))
+        #print(json.dumps(minstops))
         return routes
-
         # C) A list of the stops that connect two or more subway routes along with the relevant route and the names for each of those stops
 
     # Extend your program again such that the user can provide any two stops on the subway routes
-
     # List a rail route you could travel to get from one stop to the other.
     # Davis to Kendall -> Redline
     # Ashmont to Arlington -> Redline, Greenline
-    def third_question(second_question, data):
-        start = [
-            inquirer.List('long_name',
-                                    message="What is your starting station?",
-                                    choices=["Red Line",
-                                    "Orange Line",
-                                    "Blue Line",
-                                    "Fairmount Line",
-                                    "Fitchburg Line",
-                                    "Framingham/Worcester Line",
-                                    "Franklin Line",
-                                    "Greenbush Line",
-                                    "Haverhill Line",
-                                    "Kingston/Plymouth Line",
-                                    "Lowell Line",
-                                    "Middleborough/Lakeville Line",
-                                    "Needham Line",
-                                    "Newburyport/Rockport Line",
-                                    "Providence/Stoughton Line",
-                                    "Foxboro Event Service"],
-                                    ),
-        ]
-        start = inquirer.prompt(start)
-        #print start["long_name"]
-        end = [
-            inquirer.List('long_name',
-                                    message="What is your ending station?",
-                                    choices=["Red Line",
-                                    "Orange Line",
-                                    "Blue Line",
-                                    "Fairmount Line",
-                                    "Fitchburg Line",
-                                    "Framingham/Worcester Line",
-                                    "Franklin Line",
-                                    "Greenbush Line",
-                                    "Haverhill Line",
-                                    "Kingston/Plymouth Line",
-                                    "Lowell Line",
-                                    "Middleborough/Lakeville Line",
-                                    "Needham Line",
-                                    "Newburyport/Rockport Line",
-                                    "Providence/Stoughton Line",
-                                    "Foxboro Event Service"],
-                                    ),
-                ]
-        end = inquirer.prompt(end)
-        #print end["long_name"]
-        # now I have start and ending stations, and all I need now is to pull the stops for those two stations
-        print(second_question(start))
-        print(second_question(end))
+    def third_question(data):
+        A = input("What is the first stop? ")
+        B = input("What is the second stop? ")
+        # now I have start and target station, and all I need now is to pull the stops for all stations
+        # and cross those against my starting and ending stations to figure out how to get from point A to B
+        stops = second_question(data) # can use second method for this
+        def find_next_route(A, B, already_path=[]):
+            connected_paths = [] # need an array to store the connected paths
+            direct=[] # need an array to track if A and B are direct
 
+        print(find_next_route(A, B))
 
-    print(first_question(data))
-    second_question(data)
-    #third_question(second_question, data)
+    #print(first_question(data))
+    #second_question(data)
+    third_question(data)
